a16d10c1c2d3c3c12c53a88cdb2c09cd7794db74
Seán Gravener <sean@gravener.net>
2020-11-23 11:08:43 -0500

    Add cycles option

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-const fizzBuzz = class {
+const FizzBuzz = class {
   get answerList() {
     return this._answers.join("<br />");
   }
 
-  constructor(fizz = 3, buzz = 5) {
+  constructor(fizz = 3, buzz = 5, cycles = 100) {
     this._answers = [];
     this.fizz = fizz;
     this.buzz = buzz;
 
-    this.compute();
+    this.compute(cycles);
   }
 
-  compute() {
-    for (let i = 1; i <= 100; i++) {
-      const localAnswers = [];
-
-      if (i % this.fizz === 0) {
-        localAnswers.push("Fizz");
-      }
-
-      if (i % this.buzz === 0) {
-        localAnswers.push("Buzz");
-      }
-
-      if (!localAnswers.length) {
-        localAnswers.push(i);
-      }
-      this._answers.push(localAnswers.join(""));
+  compute(cycles) {
+    for (let i = 1; i <= cycles; i++) {
+      this._answers.push(this.toggleFizzBuzz(i));
     }
+  }
+
+  toggleFizzBuzz(i) {
+    const localAnswers = [];
+
+    if (i % this.fizz === 0) {
+      localAnswers.push("Fizz");
+    }
+
+    if (i % this.buzz === 0) {
+      localAnswers.push("Buzz");
+    }
+
+    if (!localAnswers.length) {
+      localAnswers.push(i);
+    }
+
+    return localAnswers.join("");
   }
 };
 
-const fb = new fizzBuzz();
+const fb = new FizzBuzz();
 document.getElementById("app").innerHTML = `<p>answer:</p> ${fb.answerList}`;
