da1abf3459a325254de70b21c6a2cb91dc9e3a3e
Seán Gravener <sean@gravener.net>
2022-04-13 12:55:31 -0400

    Add instructions

---
+++ src/README.md
@@ -0,0 +1,14 @@
+# A FizzBuzz Solution
+
+## Exercise Instructions:
+Print integers 1 to N, but print “Fizz” if an integer is divisible by 3, “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is divisible by both 3 and 5.
+
+## To run
+
+In terminal:
+```bash
+node src/index.js
+```
+
+In browser:
+Open index.html
--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
+/**
+ * The FizzBuzz problem. Print integers 1 to N, but print “Fizz” if an
+ * integer is divisible by 3, “Buzz” if an integer is divisible by 5,
+ * and “FizzBuzz” if an integer is divisible by both 3 and 5.
+ */
+
 const FizzBuzz = class {
   get answerList() {
     return this._answers.join("<br />");
   }
 
   constructor(fizz = 3, buzz = 5, cycles = 100) {
+    this._data = this.getData(cycles);
     this._answers = [];
     this.fizz = fizz;
     this.buzz = buzz;
 
     this.compute(cycles);
+    this.computeWithReduce();
+  }
+
+  getData(cycles) {
+    const data = [];
+    for (let i = 1; i <= cycles; i++) {
+      data.push(i);
+    }
+
+    return data;
   }
 
   compute(cycles) {
     for (let i = 1; i <= cycles; i++) {
       this._answers.push(this.toggleFizzBuzz(i));
     }
+  }
+
+  computeWithReduce() {
+    const localAnswer = this._data.reduce((currentAnswer, currentNumber) => {
+      if (currentNumber % 15 === 0) return `${currentAnswer}FizzBuzz\n`;
+      if (currentNumber % 5 === 0) return `${currentAnswer}Fizz\n`;
+      if (currentNumber % 3 === 0) return `${currentAnswer}Buzz\n`;
+
+      return `${currentAnswer}${currentNumber}\n`;
+    }, "");
+
+    console.log(localAnswer);
   }
 
   toggleFizzBuzz(i) {
